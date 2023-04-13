da511f2f3daeddb50c1a566ebc3daf09c127feb1
Waryen <jonathangomand@gmail.com>
2023-04-13 18:17:05 +0200

    feat: add content and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Counter from './components/Counter';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const resetCallback = () => {
-    setCount(0);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prev) => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div>
-      <h1>waryen.dev</h1>
-      <p>
-        You have been here for {count} {count == 0 ? 'second' : 'seconds'}.
-      </p>
-      <button onClick={resetCallback}>Reset the timer</button>
+    <div className="flex grow flex-col items-center py-10 md:py-20 px-5 md:px-10">
+      <h1 className="text-red-600 text-5xl md:text-7xl font-bold capitalize mb-10">
+        waryen.dev
+      </h1>
+      <div className="flex flex-col items-center mb-10">
+        <p className="text-xl md:text-2xl mb-3">Hello world! 👋</p>
+        <p className="text-md text-center md:text-xl mb-2">
+          My name is Jonathan and I'm a software engineer working at{' '}
+          <a
+            href="https://mirahi.io"
+            target="_blank"
+            className="text-red-600 hover:text-red-400 focus:text-red-400"
+          >
+            Mirahi.io
+          </a>
+        </p>
+        <p className="text-md md:text-xl">
+          Checkout my{' '}
+          <a
+            target="_blank"
+            className="text-red-600 hover:text-red-400 focus:text-red-400"
+            href="https://github.com/Waryen"
+          >
+            Github
+          </a>{' '}
+          and my{' '}
+          <a
+            target="_blank"
+            className="text-red-600 hover:text-red-400 focus:text-red-400"
+            href="https://twitter.com/aka_waryen"
+          >
+            Twitter
+          </a>
+        </p>
+      </div>
+      <Counter />
     </div>
   );
 }
